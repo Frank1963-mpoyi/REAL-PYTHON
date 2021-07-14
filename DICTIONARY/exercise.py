d44f652e5d50de56e945ffe43e55e0ab2f7898e4
FRANK MPOYI <68503196+Frank1963-mpoyi@users.noreply.github.com>
2021-07-14 06:26:16 +0200

    update

---
+++ DICTIONARY/exercise.py
@@ -0,0 +1,48 @@
+
+def get_input(i):
+    
+
+    i['search'] = 'search',
+
+    return i
+i ={}
+
+a = get_input(i)
+
+print(a)
+
+
+i = {}
+i['search'] = 'search'
+print(i)
+
+
+
+
+
+# def input_get_input():
+
+
+#     i = {
+#         'user'          : 'user',
+#         'user_fullname' : 'Mpoyi Tshibuyi',
+#         'type'          : "Programmer",
+#         'assignee'      : "Mpoyi",
+#         'assigner'      : "Ruth",
+
+#     }
+
+#     #i = input_common.input_get_input(self, i)
+#     a = get_input(i)
+
+#     return i
+
+
+
+# result = input_get_input()
+# print(result)# this result is variable now to access the value 
+
+# print(result['type'])
+
+
+
